86b70254f8174c1b508526809d9cd3c228654b4d
Martin Vega <martin_vega@yahoo.com>
2018-10-24 19:05:20 -0500

    first commit

---
+++ TabbarApp/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabbarApp
+//
+//  Created by Pavel Bogart on 21/02/2018.
+//  Copyright © 2018 Pavel Bogart. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
